4776174495150589f626baf660575f3d1e83d38f
David Seesahaye <davidseesahaye@gmail.com>
2026-03-19 13:45:48 +0400

    init

---
+++ src/Controller/api/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/api/test', name: 'api_test')]
+class TestController extends AbstractController
+{
+    #[Route('', name: 'index', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Test endpoint',
+            'status' => 'success',
+        ]);
+    }
+
+    #[Route('', name: 'create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        return $this->json([
+            'message' => 'Resource created',
+            'data' => $data,
+        ], 201);
+    }
+}
